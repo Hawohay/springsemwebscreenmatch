4c4ff95192e94bdd6fdb99caa01cd2964ac4c69f
Hawohay <hawohay@outlook.com>
2024-10-03 18:27:26 -0300

    Adicionando todos os arquivos do projeto

---
+++ src/main/java/com/springsemwebscreenmatch/SpringsemwebscreenmatchApplication.java
@@ -0,0 +1,13 @@
+package com.springsemwebscreenmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsemwebscreenmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsemwebscreenmatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsemwebscreenmatch/SpringsemwebscreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsemwebscreenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsemwebscreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
